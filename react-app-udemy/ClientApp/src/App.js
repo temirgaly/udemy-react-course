b5e9b9b5c7127341f782a376a50a5e326d8d96c1
dinmukhamed <dinmuhamed93@gmail.com>
2020-08-13 23:48:21 +0600

    124. Creating a Layout Component.

--- react-app-udemy/ClientApp/src/App.js
+++ react-app-udemy/ClientApp/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <h1>Hello world</h1>
+        <Layout>
+          <p>test</p>
+        </Layout>
       </div>
     );
   }
