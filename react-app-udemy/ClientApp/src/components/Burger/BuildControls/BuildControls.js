0eae9df9d5750291e8f81465b7249934a9021813
dinmukhamed <dinmuhamed93@gmail.com>
2020-08-14 22:20:28 +0600

    133. main burger components are done

---
+++ react-app-udemy/ClientApp/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.module.css';
+
+const controls = [
+    { label: 'Salad', type: 'salaf' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
+    </div>
+);
+
+export default buildControls;
