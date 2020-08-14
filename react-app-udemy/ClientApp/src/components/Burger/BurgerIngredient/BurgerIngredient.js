acc7321385d81b84032c00429858489151014720
dinmukhamed <dinmuhamed93@gmail.com>
2020-08-14 17:49:33 +0600

    126. Starting to create burger module

---
+++ react-app-udemy/ClientApp/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import clasess from './BurgerIngredient.module.css';
+
+const burgerIngredient = (props) => {
+    let ingredient = null;
+
+    switch (props.type) {
+        case ('bread-bottom'):
+            ingredient = <div className={clasess.BreadBottom}></div>;
+            break;
+        case ('bread-top'):
+            ingredient = (
+                <div className={clasess.BreadTop}>
+                    <div className={clasess.Seeds1}></div>
+                    <div className={clasess.Seeds2}></div>
+                </div>
+            );
+            break;
+        case ('meat'):
+            ingredient = <div className={clasess.Meat}></div>;
+            break;
+        case ('cheese'):
+            ingredient = <div className={clasess.Cheese}></div>;
+            break;
+        case ('bacon'):
+            ingredient = <div className={clasess.Bacon}></div>;
+            break;
+        case ('salad'):
+            ingredient = <div className={clasess.Salad}></div>;
+            break;
+        default:
+            ingredient = null;
+    }
+
+    return ingredient;
+};
+
+export default burgerIngredient;
--- react-app-udemy/ClientApp/src/components/Layout/Layout.js
+++ react-app-udemy/ClientApp/src/components/Layout/Layout.js
@@ -4,9 +4,6 @@ import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
-        {
-            console.log(classes.Content)
-        }
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
