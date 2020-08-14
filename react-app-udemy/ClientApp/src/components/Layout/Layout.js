1830d5b36a8a5e5256abf162fe936f5a7cea2371
dinmukhamed <dinmuhamed93@gmail.com>
2020-08-14 17:17:56 +0600

    fix issues with css modules. Solution: in new version create-react-app upper version 2.0.0 modules already setup. No need to eject. Just after name of css, should be added .module.css

--- react-app-udemy/ClientApp/src/components/Layout/Layout.js
+++ react-app-udemy/ClientApp/src/components/Layout/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
+        {
+            console.log(classes.Content)
+        }
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
