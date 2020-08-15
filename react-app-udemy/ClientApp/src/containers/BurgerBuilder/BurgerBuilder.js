c76f353ae5c7f7ec48832b4c099e4810bf11237f
dinmukhamed <dinmuhamed93@gmail.com>
2020-08-15 10:04:59 +0600

    141. purchase continue | purchase cancel

--- react-app-udemy/ClientApp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-app-udemy/ClientApp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,13 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: true });
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue');
+    }
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -84,8 +91,12 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
